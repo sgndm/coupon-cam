e8679eff24b7299abbf62c361a74c43b77e2cfc6
sgndm <ransika@gmail.com>
2018-07-16 16:40:41 +0530

    updated converter.php

--- app/Classes/Converter.php
+++ app/Classes/Converter.php
@@ -2,9 +2,9 @@
 namespace App\Classes;
 
 class Converter {
-    
+
     public static function currencyConverter($from, $to, $amount){
-        
+
         $url = file_get_contents('https://free.currencyconverterapi.com/api/v5/convert?q=' . $from . '_' . $to . '&compact=ultra');
         $json = json_decode($url, true);
         $rate = implode(" ",$json);
@@ -13,11 +13,19 @@ class Converter {
         $rounded = floor($total); // round to an int |ex: 1.4 -> 1, 1.7 -> 1
         return $rounded; //return converted value
     }
-    
-    
-    
+
+    public static function get_server_time_by_store_time($store_time, $store_offset, $server_offset) {
+        $temp_server_time = ($store_time + $server_offset - $store_offset);
+
+        $server_time = date('Y-m-d H:i:s', $temp_server_time);
+
+        return($server_time);
+    }
+
+
+
     public static function get_time_zone($latitude, $longitude){
-        
+
         $api_key = 'AIzaSyBAGhaRi_7Fpl9z49bzX-BRyAt0h00mHAE';
 
         $url = file_get_contents('https://maps.googleapis.com/maps/api/timezone/json?location=' . $latitude . ',' . $longitude . '&timestamp=1524138098&key=' . $api_key);
@@ -25,53 +33,53 @@ class Converter {
 
         $dayLightSaving = $json['dstOffset'];
         $offset = $json['rawOffset'];
-        
+
         $return = ($dayLightSaving + $offset);
 
         //return(array('dayLightSaving' => $dayLightSaving, 'timeOffset' => $offset));
         return $return;
     }
-    
+
     public static function calculate_server_time($store_offset, $server_offset){
         // get gmt time for store at 12.00 pm
-        $temp_time = (( 24 * 3600 ) - $store_offset); // gnt time for 12 pm
-        
+        $temp_time = (( (24 * 3600) - 10 ) - $store_offset); // gnt time for 12 pm
+
         // get server time
         $temp_server_time = ($temp_time + $server_offset);
-        
+
         $h = floor($temp_server_time / 3600); // hours
         $i = floor( ($temp_server_time % 3600) / 60 ); // minutes
         $s = (($temp_server_time % 3600) % 60);
-        
+
         $make_time = mktime($h, $i, $s);
-        
+
         $server_time = date('H:i:s', $make_time);
-        
+
         return($server_time);
-    } 
-    
+    }
+
     public static function get_server_time_by_local_time($store_time, $store_offset, $server_offset){
         // get store time in seconds
         $stm = explode(':', $store_time);
         $H = (int)$stm[0];
         $I = (int)$stm[1];
         $S = (int)$stm[2];
-        
+
         $time_in_sec = ($H * 3600) + ($I * 60) + ($S);
-        
-        // 
+
+        //
         $temp_server_time = (($time_in_sec - $store_offset) + $server_offset);
-        
+
         $h = floor($temp_server_time / 3600); // hours
         $i = floor( ($temp_server_time % 3600) / 60 ); // minutes
         $s = (($temp_server_time % 3600) % 60);
-        
+
         $make_time = mktime($h, $i, $s);
-        
+
         $server_time = date('H:i:s', $make_time);
-        
+
         return($server_time);
-        
+
     }
 
     public static function get_server_new_time($store_time, $store_offset, $server_offset) {
@@ -83,11 +91,11 @@ class Converter {
 
         return $server_time;
     }
-    
+
     public static function get_server_location(){
-        
+
         $ip = '18.130.82.43';
-        
+
         $url = file_get_contents('https://ipapi.co/' . $ip . '/json/');
         $json = json_decode($url, true);
 
@@ -96,67 +104,67 @@ class Converter {
 
         return (array('latitude' => $latitude,'longitude' => $longitude));
     }
-    
+
     public static function get_values_for_countries($st_country, $value){
-        
+
         $val_USD = 0;
         $val_CAD = 0;
         $val_NZD = 0;
         $val_AUD = 0;
         $val_UK = 0;
-        
+
         if($st_country == "GB"){
-            
+
             $base_C = 'GBP';
-            
+
             $val_USD = Converter::currencyConverter($base_C, 'USD', $value);
             $val_CAD = Converter::currencyConverter($base_C, 'CAD', $value);
             $val_NZD = Converter::currencyConverter($base_C, 'NZD', $value);
             $val_AUD = Converter::currencyConverter($base_C, 'AUD', $value);
             $val_UK = $value;
-            
+
         } elseif($st_country == "NZ"){
-            
+
             $base_C = 'NZD';
-            
+
             $val_USD = Converter::currencyConverter($base_C, 'USD', $value);
             $val_CAD = Converter::currencyConverter($base_C, 'CAD', $value);
             $val_NZD = $value;
             $val_AUD = Converter::currencyConverter($base_C, 'AUD', $value);
             $val_UK = Converter::currencyConverter($base_C, 'GBP', $value);
-            
+
         }elseif($st_country == "CA"){
-            
+
             $base_C = 'CAD';
-            
+
             $val_USD = Converter::currencyConverter($base_C, 'USD', $value);
             $val_CAD = $value;
             $val_NZD = Converter::currencyConverter($base_C, 'NZD', $value);
             $val_AUD = Converter::currencyConverter($base_C, 'AUD', $value);
             $val_UK = Converter::currencyConverter($base_C, 'GBP', $value);
-            
+
         }elseif($st_country == "AU"){
-            
+
             $base_C = 'AUD';
-            
+
             $val_USD = Converter::currencyConverter($base_C, 'USD', $value);
             $val_CAD = Converter::currencyConverter($base_C, 'CAD', $value);
             $val_NZD = Converter::currencyConverter($base_C, 'NZD', $value);
             $val_AUD = $value;
             $val_UK = Converter::currencyConverter($base_C, 'GBP', $value);
-            
+
         }else{
-            
+
             $base_C = 'USD';
-            
+
             $val_USD = $value;
             $val_CAD = Converter::currencyConverter($base_C, 'CAD', $value);
             $val_NZD = Converter::currencyConverter($base_C, 'NZD', $value);
             $val_AUD = Converter::currencyConverter($base_C, 'AUD', $value);
             $val_UK = Converter::currencyConverter($base_C, 'GBP', $value);
-            
+
         }
-        
+
         $return = [
             'val_usd' => $val_USD,
             'val_cad' => $val_CAD,
@@ -164,8 +172,8 @@ class Converter {
             'val_aud' => $val_AUD,
             'val_uk' => $val_UK,
         ];
-        
+
         return $return;
     }
-    
+
 }
